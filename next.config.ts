596fc2a8aa009c0c073bf4ec810b77eedafd6e50
purushottam-io <23f3000982@ds.study.iitm.ac.in>
2025-10-15 21:25:10 +0530

    added alias

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      // Rewrite all /id/... to /domain/...
+      {
+        source: '/domain/:path*',
+        destination: '/id/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
