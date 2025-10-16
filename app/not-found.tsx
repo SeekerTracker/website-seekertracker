c7bbeaabee73206bffaf5e3ff5fb937689dfe8f8
purushottam-io <23f3000982@ds.study.iitm.ac.in>
2025-10-16 17:26:21 +0530

    added 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,3 +1,19 @@
-export default function NotFound() {
-    return <div>fgdhj</div>
-}
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+
+const Custom404 = () => {
+    return (
+        <div
+            style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "80vh",
+            }}
+        >
+            <img src="/error.svg" alt="Error Image" width="600px" />
+        </div>
+    );
+};
+
+export default Custom404;
